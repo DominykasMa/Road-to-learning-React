04a831bd9a9d8cf79ac3422ce116773efc80b887
DominykasMa <dominykas.matutis@gmail.com>
2019-05-09 00:19:31 +0300

    Starting with lists

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectId: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectId: 1,
+  }
+];
+
 class App extends Component {
   render() {
-    var helloWorld = 'Welcome to the Road to learn React';
+    const helloWorld = 'Welcome absa';
     return (
       <div className="App">
-      <h2>{helloWorld}</h2>
+      {list.map(function(item) {
+        return <div> {item.title} </div>;
+      })}
       </div>
     );
   } 
