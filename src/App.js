2a8e0993dda440c8c634197dad30851237fb16dd
Dominykas Matutis <Domas@ad.ktdm.lt>
2019-05-09 13:15:49 +0300

    Updates

--- src/App.js
+++ src/App.js
@@ -22,15 +22,21 @@ const list = [
 
 class App extends Component {
   render() {
-    const helloWorld = 'Welcome absa';
     return (
       <div className="App">
-      {list.map(function(item) {
-        return <div> {item.title} </div>;
-      })}
+        {list.map(item => 
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </div>
+        )}
       </div>
     );
-  } 
+  }
 }
 
 export default App;
